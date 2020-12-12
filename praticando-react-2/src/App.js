c6ef98b4f9008209d8ad78330fcf23917da7ab5a
Matheus de Miranda Ferreira <ohhhmiranha@gmail.com>
2020-12-12 17:53:40 -0300

    Aprendendo ReactJS na prática

---
+++ praticando-react-2/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import TodoItem from './components/todoList/todoItem'
+class App extends Component {
+    render() {
+        return (
+            <div className="App" />
+        )
+    }
+}
+
+export default App;
