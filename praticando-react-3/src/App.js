f954732838db41eb669853943e4c144d00144676
Matheus de Miranda Ferreira <ohhhmiranha@gmail.com>
2020-12-20 16:20:58 -0300

    Mini Projetos

---
+++ praticando-react-3/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './main.css';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello world</h1>
+        )
+    }
+}
